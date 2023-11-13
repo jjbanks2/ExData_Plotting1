74981a69fe9368a554a99b3183e8e7560771f2d6
jjbanks2 <jenbanks2@gmail.com>
2023-11-13 10:44:08 -0800

    add hist and export to png

--- plot1.R
+++ plot1.R
@@ -1,17 +1,19 @@
 #Plot 1 - Global Active Power
+
 library(dplyr)
+library(datasets)
 
-#data<-read.table("household_power_consumption.txt", sep=";", na.strings="?")
+# read in file
+data<-read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
 
+# subset to relevant dates
+data<-data %>%
+    filter(Date == "1/2/2007" | Date== "2/2/2007")
 
-#not working, still in char class
-#data[['V2']]<-as.POSIXlt(data[['V2']], format="%H:%M:%S")
-#data[['V2']]<-format(as.POSIXlt(data[['V2']]), format="%H:%M:%S")
+#draw hist to screen
+hist(data$Global_active_power, xlab='Global Active Power (kilowatts)', 
+     ylab='Frequency', main="Global Active Power", col=2)
 
-#Convert date column to date format
-data[['V1']]<-as.Date(data[['V1']], format="%d/%m/%Y")
-d1<-data %>%
-    filter(data$V1 == "2007-02-01" | data$V1=="2007-02-02")
-colnames(d1)<- c("Date", "Time", "Global_active_power", "Global_reactive_power", 
-                 "Voltage", "Global-intensity", "Sub-metering_1",
-                 "Sub_metering_2", "Sub_metering_3")
+#export to png file and close device
+dev.copy(png, file="plot1.png")
+dev.off()
