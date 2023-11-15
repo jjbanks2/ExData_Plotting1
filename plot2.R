28def382c831623913f41ef5fd4ab0057fd4ce73
jjbanks2 <jenbanks2@gmail.com>
2023-11-15 15:57:48 -0800

    Done except for day of week tick marks

--- plot2.R
+++ plot2.R
@@ -1,26 +1,25 @@
 #plot2.R
 
-#library(dplyr)
-#library(datasets)
+library(dplyr)
+library(datasets)
 
 # read in file
-#d<-read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
+d<-read.table("household_power_consumption.txt", sep=";", header=TRUE, 
+ na.strings="?")
 
 # subset to relevant dates
-#d<-d %>%
-#    filter(Date == "1/2/2007" | Date== "2/2/2007")
+d<-d %>%
+    filter(Date == "1/2/2007" | Date== "2/2/2007")
 
-#Create new date-time column, combining first two columns
-#d$DateTime<-strptime(paste(d$Date, d$Time), "%d/%m/%Y %H:%M:%S")
+
+d$DateTime <- strptime(paste(d$Date,d$Time),"%d/%m/%Y %H:%M:%S")
 
 #Convert date column to date format
 #d[['Date']]<-as.Date(d[['Date']], format="%d/%m/%Y")
 
+with(d, plot(d$DateTime, d$Global_active_power,type="l",
+      ylab="Global Active Power (kilowatts)",xlab=""))
 
-
-with(d, plot(DateTime, Global_active_power, 
-             xlab="",
-             ylab="Global Active Power (kilowatts)", 
-             type="l"))
-
-
+#export to png file and close device
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
