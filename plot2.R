4505cd7dbb4ed0117a9a0e7d5448ffd46f650608
jjbanks2 <jenbanks2@gmail.com>
2023-11-16 11:20:47 -0800

    Converted x axis to weekdays!

--- plot2.R
+++ plot2.R
@@ -5,21 +5,30 @@ library(datasets)
 
 # read in file
 d<-read.table("household_power_consumption.txt", sep=";", header=TRUE, 
- na.strings="?")
+    na.strings="?")
 
 # subset to relevant dates
 d<-d %>%
     filter(Date == "1/2/2007" | Date== "2/2/2007")
 
 
-d$DateTime <- strptime(paste(d$Date,d$Time),"%d/%m/%Y %H:%M:%S")
+#Convert gap to numeric
+d$Global_active_power<-as.numeric(as.character(d$Global_active_power))
 
-#Convert date column to date format
-#d[['Date']]<-as.Date(d[['Date']], format="%d/%m/%Y")
+#Combine date and time columns
+d$DateTime<-strptime(paste(d$Date,d$Time),"%d/%m/%Y %H:%M:%S")
 
-with(d, plot(d$DateTime, d$Global_active_power,type="l",
-      ylab="Global Active Power (kilowatts)",xlab=""))
+#Draw plot w/lines and no tick mark labels
+with(d, plot(d$DateTime, d$Global_active_power,
+            type="l",
+            ylab="Global Active Power (kilowatts)",xlab="",
+            xaxt="n"))
+
+#Create and apply weekday labels
+wday<-as.POSIXct(round(range(d$DateTime),"days"))
+axis.POSIXct(1, at=seq(wday[1],wday[2], by="days"),format="%a")
 
 #export to png file and close device
 dev.copy(png, file="plot2.png", width=480, height=480)
 dev.off()
+
