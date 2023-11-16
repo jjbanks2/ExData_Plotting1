36c8283a40bc4483a9424813458d41e01fff45f8
jjbanks2 <jenbanks2@gmail.com>
2023-11-15 17:15:59 -0800

    All done except weekday conversion

---
+++ plot4.R
@@ -0,0 +1,42 @@
+#Plot 4 
+
+library(dplyr)
+library(datasets)
+
+# read in file
+data<-read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
+
+# subset to relevant dates
+d<-data %>%
+    filter(Date == "1/2/2007" | Date== "2/2/2007")
+d$DateTime <- strptime(paste(d$Date,d$Time),"%d/%m/%Y %H:%M:%S")
+
+#Create panel set up
+par(mfrow=c(2,2))
+with(d, {
+    
+    # Global active power
+    plot(d$DateTime, d$Global_active_power,type="l", ylab="Global Active Power (kilowatts)",xlab="")
+
+    # Voltage
+    plot(d$DateTime, d$Voltage,type="l", ylab="Voltage",xlab="datetime")
+
+    #Energy submetering
+    plot(d$DateTime, d$Sub_metering_1,type="l", ylab="Energy sub metering",
+      xlab="", col="black")
+    lines(d$DateTime,d$Sub_metering_2,type="l",col="red")
+    lines(d$DateTime,d$Sub_metering_3,type="l",col="blue")
+    legend("topright", 
+        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+        lty=1,
+        lwd=3,
+        col=c("black", "red", "blue"))
+    
+    #Global reactive power
+    plot(d$DateTime, d$Global_reactive_power,type="l", 
+          ylab="Global_reactive_power", xlab="datetime")
+ })
+
+#export to png file and close device
+dev.copy(png, file="plot4.png", width=480, height=480)
+dev.off()
