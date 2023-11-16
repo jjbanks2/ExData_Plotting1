9e49e63ccfb5f8d8f2940d2f49dd5233bb14bfad
jjbanks2 <jenbanks2@gmail.com>
2023-11-15 17:17:27 -0800

    Uncommenting read lines

--- plot3.R
+++ plot3.R
@@ -4,11 +4,11 @@ library(dplyr)
 library(datasets)
 
 # read in file
-#data<-read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
+data<-read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
 
 # subset to relevant dates
-#d<-data %>%
-#    filter(Date == "1/2/2007" | Date== "2/2/2007")
+d<-data %>%
+    filter(Date == "1/2/2007" | Date== "2/2/2007")
 d$DateTime <- strptime(paste(d$Date,d$Time),"%d/%m/%Y %H:%M:%S")
 #d<-data.frame(d$Sub_metering_1, d$Sub_metering_2, d$Sub_metering_3, d$DateTime)
 
@@ -21,7 +21,7 @@ lines(d$DateTime,d$Sub_metering_3,type="l",col="blue")
 legend("topright", 
        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        lty=1,
-       lwd=2.5,
+       lwd=2,
        col=c("black", "red", "blue")) 
 
 
